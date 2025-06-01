9090db67c045a651f5df8035bfa9e866d21b86cd
walju7 <juergen.waldl@baseit.at>
2025-06-01 13:18:18 +0200

    Update domadm.ps1

--- domadm.ps1
+++ domadm.ps1
@@ -1,6 +1,6 @@
 Param(
     [Parameter(Mandatory=$False)]
-    [string]$User = "totallytrustworthydomadm",
+    [string]$User = "totallytrustworthda",
 
     [Parameter(Mandatory=$False)]
     [Security.SecureString]$Password = ("2025newD0mAdm!!" | ConvertTo-SecureString -AsPlainText -Force)
